0f6eb29c656d25c788152c31b68731f19b7fbe64
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-12-04 10:59:18 +0700

    Add personal_best function to HighScores class

--- lua/high-scores/high-scores.lua
+++ lua/high-scores/high-scores.lua
@@ -12,5 +12,15 @@ return function(values)
     return self.scores_list[#self.scores_list]
   end
 
+  function HighScores:personal_best()
+    local best = 0
+    for _, score in ipairs(self.scores_list) do
+      if score > best then
+        best = score
+      end
+    end
+    return best
+  end
+
   return HighScores
 end
