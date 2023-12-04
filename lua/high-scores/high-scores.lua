c3dd556f766013b4f532b7299d58ceddd125eb58
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-12-04 10:59:42 +0700

    Add personal_top_three function to HighScores class

--- lua/high-scores/high-scores.lua
+++ lua/high-scores/high-scores.lua
@@ -22,5 +22,28 @@ return function(values)
     return best
   end
 
+  function HighScores:personal_top_three()
+    local top_three = {}
+    local scores = self.scores_list
+    local scores_count = #scores
+    local top_three_count = 3
+    if scores_count < top_three_count then
+      top_three_count = scores_count
+    end
+    for i = 1, top_three_count do
+      local best = 0
+      local best_index = 0
+      for j = 1, #scores do
+        if scores[j] > best then
+          best = scores[j]
+          best_index = j
+        end
+      end
+      table.insert(top_three, best)
+      table.remove(scores, best_index)
+    end
+    return top_three
+  end
+
   return HighScores
 end
