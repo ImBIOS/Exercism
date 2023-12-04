9b9854a86a96936e6001f5ac1e4ce37f70b43160
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-12-04 10:49:41 +0700

    Add Armstrong number check implementation

--- lua/armstrong-numbers/armstrong-numbers.lua
+++ lua/armstrong-numbers/armstrong-numbers.lua
@@ -1,7 +1,17 @@
 local ArmstrongNumbers = {}
 
 function ArmstrongNumbers.is_armstrong_number(number)
-
+  local digits = {}
+  local sum = 0
+  local n = number
+  while n > 0 do
+    table.insert(digits, n % 10)
+    n = math.floor(n / 10)
+  end
+  for _, digit in ipairs(digits) do
+    sum = sum + digit ^ #digits
+  end
+  return sum == number
 end
 
 return ArmstrongNumbers
