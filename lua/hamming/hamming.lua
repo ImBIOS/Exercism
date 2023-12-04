27d2a7f20989e100e6acdd87106031b8543bf3ea
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-12-04 10:48:16 +0700

    Fix Hamming computation bug

--- lua/hamming/hamming.lua
+++ lua/hamming/hamming.lua
@@ -1,7 +1,18 @@
 local Hamming = {}
 
 function Hamming.compute(a,b)
+  if #a ~= #b then
+    return -1
+  end
 
+  local distance = 0
+  for i = 1, #a do
+    if a:sub(i,i) ~= b:sub(i,i) then
+      distance = distance + 1
+    end
+  end
+
+  return distance
 end
 
 return Hamming
