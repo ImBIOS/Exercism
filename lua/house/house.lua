3faf91aacdc932ce81ae9b3353cd9e8969de208f
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-12-04 11:09:20 +0700

    Update house.lua with verses and recite function

--- lua/house/house.lua
+++ lua/house/house.lua
@@ -1,11 +1,83 @@
 local house = {}
 
-house.verse = function(which)
+-- Array containing all verses
+local verses = {[[This is the house that Jack built.]], [[This is the malt
+that lay in the house that Jack built.]], [[This is the rat
+that ate the malt
+that lay in the house that Jack built.]], [[This is the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.]], [[This is the dog
+that worried the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.]], [[This is the cow with the crumpled horn
+that tossed the dog
+that worried the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.]], [[This is the maiden all forlorn
+that milked the cow with the crumpled horn
+that tossed the dog
+that worried the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.]], [[This is the man all tattered and torn
+that kissed the maiden all forlorn
+that milked the cow with the crumpled horn
+that tossed the dog
+that worried the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.]], [[This is the priest all shaven and shorn
+that married the man all tattered and torn
+that kissed the maiden all forlorn
+that milked the cow with the crumpled horn
+that tossed the dog
+that worried the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.]], [[This is the rooster that crowed in the morn
+that woke the priest all shaven and shorn
+that married the man all tattered and torn
+that kissed the maiden all forlorn
+that milked the cow with the crumpled horn
+that tossed the dog
+that worried the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.]], [[This is the farmer sowing his corn
+that kept the rooster that crowed in the morn
+that woke the priest all shaven and shorn
+that married the man all tattered and torn
+that kissed the maiden all forlorn
+that milked the cow with the crumpled horn
+that tossed the dog
+that worried the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.]], [[This is the horse and the hound and the horn
+that belonged to the farmer sowing his corn
+that kept the rooster that crowed in the morn
+that woke the priest all shaven and shorn
+that married the man all tattered and torn
+that kissed the maiden all forlorn
+that milked the cow with the crumpled horn
+that tossed the dog
+that worried the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.]]}
 
+-- Function to return a specific verse
+house.verse = function(which)
+    return verses[which]
 end
 
+-- Function to recite the whole song
 house.recite = function()
-
+    -- This function will concatenate all verses with a newline
+    -- You will implement this later
 end
 
 return house
