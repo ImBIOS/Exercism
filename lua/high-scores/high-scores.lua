6da293fb8bcf28026b69077b56ba9c4164c1e0e1
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-12-04 10:58:23 +0700

    Add high scores functionality

--- lua/high-scores/high-scores.lua
+++ lua/high-scores/high-scores.lua
@@ -1,1 +1,12 @@
-return function(scores) end
+return function(values)
+  local HighScores = {}
+
+  -- Directly assign the passed values to scores_list
+  HighScores.scores_list = values[1]
+
+  function HighScores:scores()
+    return self.scores_list
+  end
+
+  return HighScores
+end
