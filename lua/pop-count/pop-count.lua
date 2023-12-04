6ceafb9f91fd0c42917d11f1bb3839acf25e7d83
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-12-04 10:46:34 +0700

    Add pop count function to calculate the number of set bits in a given number

--- lua/pop-count/pop-count.lua
+++ lua/pop-count/pop-count.lua
@@ -1,7 +1,14 @@
 local PopCount = {}
 
 function PopCount.egg_count(number)
-
+  local count = 0
+  while number > 0 do
+    if number % 2 == 1 then
+      count = count + 1
+    end
+    number = math.floor(number / 2)
+  end
+  return count
 end
 
 return PopCount
